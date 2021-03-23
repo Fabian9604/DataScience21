b27ef8d057c8ea5367b3cbdbafb12f0a15c22607
Fabian Gerock <fgerock@hs-offenburg.de>
2021-03-23 12:32:44 +0100

    vor letzter probe

--- Block_0/Assignments/TheBestModule.py
+++ Block_0/Assignments/TheBestModule.py
@@ -15,11 +15,10 @@ class ListKeeper(dict):
     # delete is able to delete a list in the ListKeeper object, specified by its name
     def delete(self,name):
         self.pop(name)
-    # sort returns the names of the lists, but sorted. It does no influence the list in the ListKeeper
-    def sortit(self,name):
+    # sort returns the specified list, but sorted. It does no influence the list in the ListKeeper
+    def sort(self,name):
         sorted_list = self[name][:]
         sorted_list.sort()
-        print(self)
         return (sorted_list)
     # append extends the specified list with the new_list
     def append(self,name,new_list):
